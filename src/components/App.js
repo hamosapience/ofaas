d6f086dda8283bb0f26818f6578c6ca1c09758fd
Kalentine Vamenek <aisseday@gmail.com>
2016-03-09 01:38:33 +0300

    initial commit

---
+++ src/components/App.js
@@ -0,0 +1,12 @@
+import React, { PropTypes } from 'react';
+// import FuelSavingsPage from '../containers/FuelSavingsPage';
+import AddTask from './AddTask';
+const App = () => {
+  return (
+    <div>
+        <AddTask />
+    </div>
+  );
+};
+
+export default App;
---
+++ src/index.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import {render} from 'react-dom';
+import { Provider } from 'react-redux';
+import { Router, browserHistory } from 'react-router';
+import configureStore from './store/configureStore';
+import './styles/styles.scss'; //Yep, that's right. You can import SASS/CSS files too! Webpack will run the associated loader and plug this into the page.
+import App from './components/App';
+
+const store = configureStore();
+
+render(
+  <Provider store={store}>
+      <App />
+  </Provider>, document.getElementById('app')
+);
